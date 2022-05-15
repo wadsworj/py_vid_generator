9049102ff178b21401af2aee93cec7f3b4d1eb67
James Wadsworth <jwadsworth@transplus.io>
2022-05-15 13:46:46 -0400

    pass font size and font type in via data file

--- elements/textElement.py
+++ elements/textElement.py
@@ -6,7 +6,7 @@ import config
 
 
 class TextElement:
-    def __init__(self, name, text, position, bounding_box, duration):
+    def __init__(self, name, text, position, bounding_box, duration, font_size, font_type):
         self.name = name
         self.video = None
         self.position = position
@@ -14,13 +14,15 @@ class TextElement:
         self.duration = duration
         self.bounding_box = bounding_box
         self.count = 0
+        self.font_size = font_size
+        self.font_type = font_type
 
     def render(self, screen):
         length = len(self.text)
 
-        current = math.floor(self.count * (length) / (self.duration * config.FRAME_RATE))
+        current = math.floor(self.count * length / (self.duration * config.FRAME_RATE))
 
-        my_font = pygame.font.SysFont('Comic Sans MS', 30)
+        my_font = pygame.font.SysFont(self.font_type, self.font_size)
         text_surface = my_font.render(self.text[0:current], False, config.WHITE)
         screen.blit(text_surface, self.position)
         self.count = self.count + 1
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame, sys, os
 import pygame.camera
 from pygame.locals import *
@@ -11,15 +13,37 @@ pygame.init()
 pygame.camera.init()
 pygame.font.init()  # you have to call this at the start,
 
-video = Video("test", (1280, 720))
 
-scene = Scene("test", 0)
+# scene = Scene("test", 0)
 
 #def __init__(self, name, text, position, bounding_box, duration):
-text_element = TextElement("first", "text test", (50,50), (400, 700), 3)
+# text_element = TextElement("first", "text test", (50,50), (400, 700), 3)
 
-scene.add_element(text_element)
+# scene.add_element(text_element)
 
-video.add_scene(scene)
+# video.add_scene(scene)
+
+# video.render()
+
+with open('videoScripts/rabinKarp.json') as data_file:
+    data = json.load(data_file)
+
+video = Video(data["name"], data["resolution"])
+
+for data_scene in data["scenes"]:
+    scene = Scene(data_scene["scene_name"], data_scene["scene_index"])
+    for data_element in data_scene["elements"]:
+        # def __init__(self, name, text, position, bounding_box, duration):
+        element = TextElement(data_element["name"],
+                              data_element["text"],
+                              data_element["position"],
+                              data_element["bounding_box"],
+                              data_element["duration"],
+                              data_element["font_size"],
+                              data_element["font_type"])
+
+        scene.add_element(element)
+
+    video.add_scene(scene)
 
 video.render()
